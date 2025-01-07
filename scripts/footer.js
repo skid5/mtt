495ddbccb4d3dcd7be91f44fb7a25d81d06b2524
skid5 <kettu.posti@outlook.com>
2025-01-07 17:36:59 +0200

    changes based on feedback

--- scripts/footer.js
+++ scripts/footer.js
@@ -1,5 +1,6 @@
 document.write(`
     <footer>
-        <p>&copy; Matin Terveyspalvelu 2017. Kaikki oikeudet pidätetään.</p>
+        <p>Matin Terveyspalvelu, 2472751-1, Mustapellonpolku 3, 044 555 8224, matti.kettunen@kotiportti.fi</p>
+        <p>&copy; 2017 Kaikki oikeudet pidätetään.</p>
     </footer>
 `);
