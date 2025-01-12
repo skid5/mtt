caaaec0c6a69feaeecc4e0c43df022e7877d9d5f
Heikki Kettunen <mmkki@Heikkis-Mac-Studio.local>
2025-01-12 19:26:39 +0200

    changed the year to 2025

--- scripts/footer.js
+++ scripts/footer.js
@@ -1,6 +1,6 @@
 document.write(`
     <footer>
         <p>Matin Terveyspalvelu, 2472751-1, Mustapellonpolku 3, 044 555 8224, matti.kettunen@kotiportti.fi</p>
-        <p>&copy; 2017 Kaikki oikeudet pidätetään.</p>
+        <p>&copy; 2025 Kaikki oikeudet pidätetään.</p>
     </footer>
 `);
